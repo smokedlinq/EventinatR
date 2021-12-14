33c7dcf031a598b699b6aa2aea30a96b68a00f2e
Adam Weigert <smokedlinq@outlook.com>
2021-12-13 20:35:59 -0500

    added base vertex and edge classes

---
+++ src/EventinatR.Projections.Graph.Cosmos/Edge.cs
@@ -0,0 +1,7 @@
+﻿namespace EventinatR.Projections.Graph.Cosmos;
+
+public abstract class Edge
+{
+    public string? Id { get; set; }
+    public string? Label { get; set; }
+}
---
+++ src/EventinatR.Projections.Graph.Cosmos/Vertex.cs
@@ -0,0 +1,8 @@
+﻿namespace EventinatR.Projections.Graph.Cosmos;
+
+public abstract class Vertex
+{
+    public string? Id { get; set; }
+    public string? Label { get; set; }
+    public string PartitionKey { get; set; } = "PartitionKey";
+}
